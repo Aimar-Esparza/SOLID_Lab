be2a1983129495760236e777c7d63b56885db8f3
Aimar <Aimar@Aimar-PC>
2025-10-18 13:03:59 +0200

    SRP update

--- SOLID_Lab/src/srp/DedukzioCalc.java
+++ SOLID_Lab/src/srp/DedukzioCalc.java
@@ -3,7 +3,11 @@ package srp;
 public class DedukzioCalc {
 
 	public float DedukzioaKalkulatu(float billAmount, int deductionPercentage) {
-		return (billAmount * deductionPercentage) / 100;
+		if (billAmount >50000){
+			 return (billAmount * deductionPercentage +5) / 100;
+		}else { 
+			return (billAmount * deductionPercentage) / 100;
+		}
 		
 	}
 }
